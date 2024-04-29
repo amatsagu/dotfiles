766030a6f66635a26f96f33dd0489db747e21f52
amatsagu <krzysztof.rapior@protonmail.com>
2024-04-29 14:20:29 +0200

    feat: sync

---
+++ tmp.sh
@@ -0,0 +1,6 @@
+rm $HOME/.config/sway -r
+rm $HOME/.config/waybar -r
+rm $HOME/.config/mako -r
+cp -r ./config/sway $HOME/.config/sway -R
+cp -r ./config/waybar $HOME/.config/waybar -R
+cp -r ./config/mako $HOME/.config/mako -R
