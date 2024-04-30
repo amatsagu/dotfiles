3abdf7432a2993475c4561c92c2b6e8745d4c5a2
amatsagu <krzysztof.rapior@protonmail.com>
2024-04-30 18:05:35 +0200

    fix: update spotify gpg key

--- app/install/spotify.sh
+++ app/install/spotify.sh
@@ -6,7 +6,7 @@ cat << EOF
 # ===========================================================================
 EOF
 
-curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt install apt-transport-https -y
 sudo apt-get update
