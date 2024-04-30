7d8870f1b1e95cfcc75c96dfc4fd84275632c418
amatsagu <krzysztof.rapior@protonmail.com>
2024-04-30 23:01:08 +0200

    fix: remove .deb package after being used

--- lang/go-1.22.2.sh
+++ lang/go-1.22.2.sh
@@ -8,6 +8,7 @@ EOF
 
 wget -O ~/GolangSetup.deb "https://dl.google.com/go/go1.22.2.linux-amd64.tar.gz"
 sudo tar -C /usr/local/ -xzf ~/GolangSetup.deb
+rm ~/GolangSetup.deb
 echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
 
 printf "\n\n\n\n\n\n\n\n\n\n"
