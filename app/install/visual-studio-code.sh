d0e03f4c958f346c5804a7ba94b63b7acad072e1
amatsagu <krzysztof.rapior@protonmail.com>
2024-04-30 14:34:03 +0200

    fix: Don't remove VSC handler

--- app/install/visual-studio-code.sh
+++ app/install/visual-studio-code.sh
@@ -6,19 +6,12 @@ cat << EOF
 # ===========================================================================
 EOF
 
-# wget -O ~/VSCSetup.deb "https://go.microsoft.com/fwlink/?LinkID=760868"
-# sudo apt install ~/VSCSetup.deb -y
-# rm ~/VSCSetup.deb
-# sudo cp ./desktop/code.desktop /usr/share/applications/code.desktop
-# sudo rm /usr/share/applications/code-url-handler.desktop
-
 curl -sS https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/microsoft.gpg
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 sudo apt install apt-transport-https -y
 sudo apt update
 sudo apt install code -y
 sudo cp ./desktop/code.desktop /usr/share/applications/code.desktop
-sudo rm /usr/share/applications/code-url-handler.desktop
 xdg-mime default code.desktop text/plain
 
 printf "\n\n\n\n\n\n\n\n\n\n"
