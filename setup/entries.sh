e79cf3cf7cc42b12eacdd224b3e8f7b5e4a6ccd4
amatsagu <krzysztof.rapior@protonmail.com>
2024-04-30 20:30:59 +0200

    feat: remove not needed / wrong entries

--- setup/entries.sh
+++ setup/entries.sh
@@ -8,11 +8,9 @@ EOF
 
 sudo rm /usr/share/applications/yelp.desktop # "Getting help" manual from Gnome, not needed
 sudo rm /usr/share/applications/vim.desktop # Vim text editor (we use nano or vsc) - keep in mind we only remove it from list on Mod+D list (fuzzel), not app itself
-sudo rm /usr/share/applications/org.gnome/htop.desktop # Htop has no reason to have own entry, especially that it is reported to not even work as it is terminal app
+sudo rm /usr/share/applications/htop.desktop # Htop has no reason to have own entry, especially that it is reported to not even work as it is terminal app
+sudo rm /usr/share/applications/footclient.desktop
+sudo rm /usr/share/applications/foot-server.desktop
+sudo rm /usr/share/applications/display-im6.q16.desktop
 
-# Hide those apps from app list (Mod+D, fuzzel) - they still can be accessed
-sudo echo "NoDisplay=true" >> /usr/share/applications/org.gnome/FileRoller.desktop
-sudo echo "NoDisplay=true" >> /usr/share/applications/org.gnome/footclient.desktop
-sudo echo "NoDisplay=true" >> /usr/share/applications/org.gnome/foot.desktop
-sudo echo "NoDisplay=true" >> /usr/share/applications/org.gnome/foot-server.desktop
-sudo echo "NoDisplay=true" >> /usr/share/applications/org.gnome/display-im6.q16.desktop # ImageMagic color depth viewer
+xdg-desktop-menu forceupdate
