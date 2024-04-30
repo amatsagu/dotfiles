ebbac17a7eb1f53a7aea1d85cbea9dfe678c3d57
amatsagu <krzysztof.rapior@protonmail.com>
2024-04-30 22:07:35 +0200

    feat: add optional installation support for common languages

---
+++ lang/go-1.22.2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cat << EOF
+# ===========================================================================
+# GOLANG 1.22.2 INSTALLATION
+# ===========================================================================
+EOF
+
+wget -O ~/GolangSetup.deb "https://dl.google.com/go/go1.22.2.linux-amd64.tar.gz"
+sudo tar -C /usr/local/ -xzf ~/GolangSetup.deb
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
+
+printf "\n\n\n\n\n\n\n\n\n\n"
+
+cat << EOF
+# ===========================================================================
+# FINISHED INSTALLATION
+# ===========================================================================
+EOF
