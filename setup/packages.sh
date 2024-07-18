4bd6b33c289bcd31693719afb5f0fe8d3df8b6a0
amatsagu <krzysztof.rapior@protonmail.com>
2024-07-18 16:58:40 +0200

    feat: add cantarell font to help some GTK apps properly load default font.

--- setup/packages.sh
+++ setup/packages.sh
@@ -39,7 +39,7 @@ sudo apt install libglib2.0-0 libglib2.0-bin libnotify-bin -y
 sudo apt install fuzzel grim slurp -y
 
 # Install fonts
-sudo apt install fonts-font-awesome fonts-roboto fonts-roboto-hinted fonts-roboto-unhinted fonts-roboto-fontface fonts-firacode fonts-noto fonts-noto-cjk fonts-noto-cjk-extra fonts-noto-color-emoji fonts-noto-core fonts-noto-extra fonts-noto-hinted fonts-noto-mono fonts-noto-ui-core fonts-noto-ui-extra fonts-noto-unhinted -y
+sudo apt install fonts-cantarell fonts-font-awesome fonts-roboto fonts-roboto-hinted fonts-roboto-unhinted fonts-roboto-fontface fonts-firacode fonts-noto fonts-noto-cjk fonts-noto-cjk-extra fonts-noto-color-emoji fonts-noto-core fonts-noto-extra fonts-noto-hinted fonts-noto-mono fonts-noto-ui-core fonts-noto-ui-extra fonts-noto-unhinted -y
 
 # Download & register custom set of cursors (for current user)
 mkdir ~/.local/share/icons -p
