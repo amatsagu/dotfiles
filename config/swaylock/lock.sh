ecaadb29364e8ea794a528f527a56e19fa4731ed
amatsagu <krzysztof.rapior@protonmail.com>
2024-10-10 17:29:42 +0200

    fix: configure swayidle

--- config/swaylock/lock.sh
+++ config/swaylock/lock.sh
@@ -4,17 +4,3 @@ LOCK_IMAGE=~/.config/swaylock/lockscreen.jpeg
 
 grim $LOCK_IMAGE
 convert $LOCK_IMAGE -blur 0x3 -fill black -colorize 25% $LOCK_IMAGE
-swaylock \
-    --image=$LOCK_IMAGE \
-    --ignore-empty-password \
-    --indicator-radius=150 \
-    --line-color=#68a1be \
-    --ring-color=#498bab \
-    --separator-color=#498bab \
-    --text-color=#e8eaed \
-    --layout-text-color=#e8eaed \
-    --inside-ver-color=#dda456 \
-    --line-ver-color=#dda456 \
-    --ring-ver-color=#d58e2a \
-    --key-hl-color=#b7bdf8 \
-    --line-wrong-color=#dd5656
