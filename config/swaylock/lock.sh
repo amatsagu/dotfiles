64bc1e11c4ad2043a41ae212c8afc181e04b4549
amatsagu <krzysztof.rapior@protonmail.com>
2024-10-27 16:54:37 +0100

    feat: update wallpaper and solve bug with swayidle

--- config/swaylock/lock.sh
+++ config/swaylock/lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOCK_IMAGE=~/.config/swaylock/lockscreen.jpeg
+LOCK_IMAGE=~/.config/swaylock/lockscreen.jpg
 
 grim $LOCK_IMAGE
 convert $LOCK_IMAGE -blur 0x3 -fill black -colorize 25% $LOCK_IMAGE
