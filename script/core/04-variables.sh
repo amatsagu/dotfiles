90e82cfeca2bd1bf6b96c2bd857873e7829241d0
amatsagu <krzysztof.rapior@protonmail.com>
2025-03-09 20:02:28 +0000

    Mar 9, 2025, 9:02 PM

--- script/core/04-variables.sh
+++ script/core/04-variables.sh
@@ -13,6 +13,28 @@ export ELECTRON_OZONE_PLATFORM_HINT=wayland
 export QT_QPA_PLATFORM=wayland
 export GTK_CSD=0
 
+# Check if system is running in virtual machine
+case "$(systemd-detect-virt)" in
+qemu)
+  export WLR_RENDERER=pixman
+  export WLR_NO_HARDWARE_CURSORS=1
+  ;;
+kvm)
+  export WLR_NO_HARDWARE_CURSORS=1
+  ;;
+oracle)
+  export WLR_NO_HARDWARE_CURSORS=1
+  ;;
+esac
+
+# Apply Nvidia-specific variables
+if [ -d /sys/module/nvidia ] && [ ! -d /sys/module/amdgpu ] && [ ! -d /sys/module/i915 ]; then
+    export WLR_NO_HARDWARE_CURSORS=1
+    export GBM_BACKEND=nvidia-drm
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export LIBVA_DRIVER_NAME=nvidia
+fi
+
 # Auto start sway from tty1 after login
 if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
     exec sway
