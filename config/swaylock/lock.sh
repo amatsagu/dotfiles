d227da28039fbf6500702d4d4cc88b97c219284c
amatsagu <krzysztof.rapior@protonmail.com>
2025-03-09 20:48:55 +0000

    Mar 9, 2025, 9:48 PM

--- config/swaylock/lock.sh
+++ config/swaylock/lock.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+SCREENSHOT_IMAGE=/tmp/screen.jpg
 LOCK_IMAGE=~/.config/swaylock/lockscreen.jpg
 
-grim $LOCK_IMAGE
-#convert $LOCK_IMAGE -blur 0x5 -fill black -colorize 25% $LOCK_IMAGE
-ffmpeg -i $LOCK_IMAGE -vf "gblur=sigma=10, vignette=PI/5" -c:a copy $LOCK_IMAGE
+grim $SCREENSHOT_IMAGE
+ffmpeg -i $SCREENSHOT_IMAGE -vf "gblur=sigma=10, vignette=PI/5" -c:a copy $LOCK_IMAGE
 
 swaylock -f --daemonize
