2f16cfdf02ce56380a7edcffc88379cba39301ba
amatsagu <krzysztof.rapior@protonmail.com>
2025-03-09 19:54:11 +0000

    Mar 9, 2025, 8:54 PM

--- script/core/02-sway.sh
+++ script/core/02-sway.sh
@@ -30,7 +30,7 @@ install_sway_packages() {
 	    gvfs-common \
 	    gvfs-backends \
 	    nwg-look \
-        policykit-1-gnome \
+        mate-polkit \
     -y >> /dev/null
 
     xdg-mime default foot.desktop x-scheme-handler/terminal >> /dev/null
