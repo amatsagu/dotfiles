1721b81a5db8e56bb143ea081604f5e02f1bbf49
amatsagu <81469656+amatsagu@users.noreply.github.com>
2026-04-28 11:41:54 +0200

    fix: solve typo in preferred font and remove no longer needed xargs

--- configs/sway/script/set-wallpaper.sh
+++ configs/sway/script/set-wallpaper.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 WALLPAPER_DIR="$HOME/.local/share/backgrounds"
-SELECTED_WALLPAPER=$(cat "$WALLPAPER_DIR/.current-wallpaper")
+SELECTED_WALLPAPER=$(cat "$WALLPAPER_DIR/.current-wallpaper" 2>/dev/null)
+
+if [ -z "$SELECTED_WALLPAPER" ] || [ ! -f "$WALLPAPER_DIR/$SELECTED_WALLPAPER" ]; then
+    SELECTED_WALLPAPER=$(ls -1 "$WALLPAPER_DIR" | grep -E "\.(jpg|jpeg|png)$" | head -n 1)
+fi
 
 pkill swaybg 2> /dev/null
 swaybg -i "$WALLPAPER_DIR/$SELECTED_WALLPAPER" -m fill > /dev/null 2>&1 &
