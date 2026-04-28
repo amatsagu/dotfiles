a93b0f2bb4ac5483c87a036b6398680acab7e4eb
amatsagu <81469656+amatsagu@users.noreply.github.com>
2026-04-28 11:52:12 +0200

    feat: remove redundant gsettings calls

--- scripts/shared/style.sh
+++ scripts/shared/style.sh
@@ -16,16 +16,18 @@ install_dotfiles() {
 }
 
 apply_gsettings() {
+    # Theme & Color Scheme
+    gsettings set org.x.apps.portal color-scheme "prefer-dark"
     gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-    # gsettings set org.x.apps.portal color-scheme "prefer-dark"
-
     gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
     gsettings set org.gnome.desktop.interface cursor-theme "phinger-cursors-dark"
     gsettings set org.gnome.desktop.interface gtk-theme "Fluent-grey-Dark"
 
+    # Localization
     gsettings set org.gnome.desktop.interface clock-format "24h"
     gsettings set org.gtk.Settings.FileChooser clock-format "24h"
 
+    # Fonts
     gsettings set org.gnome.desktop.interface font-name "Roboto 10"
     gsettings set org.gnome.desktop.interface document-font-name "Roboto 10"
     gsettings set org.gnome.desktop.wm.preferences titlebar-font "Sans Bold 10"
